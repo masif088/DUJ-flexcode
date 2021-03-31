b42f9bc1b64c2363ddea0b69a5175b557ad53113
masif088 <mokhamadasif@gmail.com>
2021-03-31 17:59:23 +0700

    update barang and barcode

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\Barang\StoreRequest;
 use App\Models\Barang;
 use App\Models\Barcode;
+use App\Models\Gudang;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Services\Barang\BarangService;
@@ -27,25 +28,45 @@ class BarangController extends Controller
                 $barcode = Barcode::where('id', $request->barcode)->with('masuk')->get();
             } else {
                 $barcode = $id->barcodes()->with('masuk')->get();
+                $barcode = $id->barcodes();
+                if ($request->gudang != null) {
+                    $barcode = $barcode->whereHas('masuk', function ($m) use ($request) {
+                        return $m->where('gudang_id', $request->gudang);
+                    });
+                }
+                $barcode = $barcode->with('masuk')->get();
             }
         }
         return view('backend.barcode', compact('barcode'));
+    }
+
+    public function detailGudang(Request $request)
+    {
+        if ($request->gudang == null) {
+            return redirect()->route('barang.index');
+        }
+        $barang = Barang::whereHas('masuk', function ($z) use ($request) {
+            return $z->where('gudang_id', $request->gudang);
+        })->orderByDesc('created_at')->paginate(30);
+        $gudang = Gudang::get(['id', 'name']);
+        return view('barang.semuabarang', compact(['barang', 'gudang']));
     }
 
     public function index(Request $request)
     {
         if (auth()->user()->role == 'admin') {
             if ($request->barang != null) {
-                $barang = Barang::where('id', $request->barang)->orderByDesc('created_at')->paginate(30);
+                $barang = Barang::has('masuk')->where('id', $request->barang)->orderByDesc('created_at')->paginate(30);
             } else {
-                $barang = Barang::orderByDesc('created_at')->paginate(30);
+                $barang = Barang::has('masuk')->orderByDesc('created_at')->paginate(30);
             }
         } else {
             $barang = Barang::whereHas('masuk', function ($z) {
                 return $z->where('gudang_id', auth()->user()->gudang_id);
             })->orderByDesc('created_at')->paginate(30);
         }
-        return view('barang.semuabarang', compact('barang'));
+        $gudang = Gudang::get(['id', 'name']);
+        return view('barang.semuabarang', compact(['barang', 'gudang']));
     }
 
     public function create()
--- resources/views/backend/barcode.blade.php
+++ resources/views/backend/barcode.blade.php
@@ -6,26 +6,26 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link rel="stylesheet" href="https://printjs-4de6.kxcdn.com/print.min.css">
-
 </head>
 <body>
-    <div>
-        <button onclick="printJS('printJS-form', 'html')">Print</button>
-    </div>
-    <div id="printJS-form">
-
-        @foreach ($barcode as $b)
+<div>
+    <button onclick="printJS('printJS-form', 'html')">Print</button>
+</div>
+<div id="printJS-form">
+    @foreach ($barcode as $b)
         <div style="display:inline-block; margin: 20px;">
             {!! QrCode::size(100)->generate($b->kode); !!}
             <p>Nama: {{$b->masuk->barang->name}}</p>
             <p>kode: {{$b->kode}}</p>
-            <p>status: {{$b->status}}</p>
-            <p>gudang:{{$b->masuk->gudang_id}}</p>
-        <p>harga: {{$b->masuk->harga_satuan}}</p>
-    </div>
+            @if (auth()->user()->role == 'admin')
+                <p>status: {{$b->status}}</p>
+                <p>gudang:{{$b->masuk->gudang_id}}</p>
+                <p>harga: {{$b->masuk->harga_satuan}}</p>
+
+            @endif
+        </div>
     @endforeach
 </div>
 <script src="https://printjs-4de6.kxcdn.com/print.min.js"></script>
-
 </body>
 </html>
